9ce2376a89226c39c9324b73e0a50e60ae6a01e8
UK-Roy <utsharoy99@gmail.com>
2025-12-29 21:44:57 +0000

    Deploying to gh-pages from @ UK-Roy/UK-Roy.github.io@e4b4066e216cdc6ec9e9897890c37c1f7ec5d4e3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "Projects",
-          description: "",
+          description: "Research and engineering projects",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
