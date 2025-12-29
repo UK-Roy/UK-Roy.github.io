742c9249f241aa12eda413a0c50e523b226612a1
UK-Roy <utsharoy99@gmail.com>
2025-12-29 11:17:56 +0000

    Deploying to gh-pages from @ UK-Roy/UK-Roy.github.io@84b0f5310603cdd1c2f4540a08ae45088ab7d83a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -455,6 +455,13 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
+        id: 'social-cv',
+        title: 'CV',
+        section: 'Socials',
+        handler: () => {
+          window.open("/assets/pdf/myAcademicCV.pdf", "_blank");
+        },
+      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
